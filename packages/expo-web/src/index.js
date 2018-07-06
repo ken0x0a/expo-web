a0a32eb960b8a67754a957de6d6bcf49e07688f5
k <k.o@plusuno.jp>
2018-07-07 04:35:35 +0900

    remove unnecessary Font export

--- packages/expo-web/src/index.js
+++ packages/expo-web/src/index.js
@@ -4,17 +4,17 @@ import DeviceInfo from './exports/DeviceInfo';
 import AppLoading from './exports/AppLoading';
 import Asset from './exports/Asset';
 import Font from './exports/Font';
-import Ionicons from './exports/Ionicons';
-import Entypo from './exports/Entypo';
-import EvilIcons from './exports/EvilIcons';
-import Feather from './exports/Feather';
-import FontAwesome from './exports/FontAwesome';
-import Foundation from './exports/Foundation';
-import MaterialIcons from './exports/MaterialIcons';
-import MaterialCommunityIcons from './exports/MaterialCommunityIcons';
-import Octicons from './exports/Octicons';
-import SimpleLineIcons from './exports/SimpleLineIcons';
-import Zocial from './exports/Zocial';
+// import Ionicons from './exports/Ionicons';
+// import Entypo from './exports/Entypo';
+// import EvilIcons from './exports/EvilIcons';
+// import Feather from './exports/Feather';
+// import FontAwesome from './exports/FontAwesome';
+// import Foundation from './exports/Foundation';
+// import MaterialIcons from './exports/MaterialIcons';
+// import MaterialCommunityIcons from './exports/MaterialCommunityIcons';
+// import Octicons from './exports/Octicons';
+// import SimpleLineIcons from './exports/SimpleLineIcons';
+// import Zocial from './exports/Zocial';
 import Constants from './exports/Constants';
 import Permissions from './exports/Permissions';
 import Notifications from './exports/Notifications';
@@ -28,17 +28,17 @@ export {
   AppLoading,
   Asset,
   Font,
-  Ionicons,
-  Entypo,
-  EvilIcons,
-  Feather,
-  FontAwesome,
-  Foundation,
-  MaterialIcons,
-  MaterialCommunityIcons,
-  Octicons,
-  SimpleLineIcons,
-  Zocial,
+  // Ionicons,
+  // Entypo,
+  // EvilIcons,
+  // Feather,
+  // FontAwesome,
+  // Foundation,
+  // MaterialIcons,
+  // MaterialCommunityIcons,
+  // Octicons,
+  // SimpleLineIcons,
+  // Zocial,
   Constants,
   Permissions,
   Notifications,
