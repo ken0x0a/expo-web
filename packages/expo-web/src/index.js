6b1c512f8dd016556bcf2c0a40841c214a8e0aa1
Ken <k.o@plusuno.jp>
2018-09-27 05:48:58 +0900

    vector-icons

--- packages/expo-web/src/index.js
+++ packages/expo-web/src/index.js
@@ -4,17 +4,6 @@ import DeviceInfo from './exports/DeviceInfo';
 import AppLoading from './exports/AppLoading';
 import Asset from './exports/Asset';
 import Font from './exports/Font';
-// import Ionicons from './exports/Ionicons';
-// import Entypo from './exports/Entypo';
-// import EvilIcons from './exports/EvilIcons';
-// import Feather from './exports/Feather';
-// import FontAwesome from './exports/FontAwesome';
-// import Foundation from './exports/Foundation';
-// import MaterialIcons from './exports/MaterialIcons';
-// import MaterialCommunityIcons from './exports/MaterialCommunityIcons';
-// import Octicons from './exports/Octicons';
-// import SimpleLineIcons from './exports/SimpleLineIcons';
-// import Zocial from './exports/Zocial';
 import Constants from './exports/Constants';
 import Permissions from './exports/Permissions';
 import Notifications from './exports/Notifications';
@@ -22,27 +11,41 @@ import Util from './exports/Util';
 import Localization from './exports/Localization';
 import WebBrowser from './exports/WebBrowser';
 
+import Ionicons from './exports/Ionicons';
+import Entypo from './exports/Entypo';
+import EvilIcons from './exports/EvilIcons';
+import Feather from './exports/Feather';
+import FontAwesome from './exports/FontAwesome';
+import Foundation from './exports/Foundation';
+import MaterialIcons from './exports/MaterialIcons';
+import MaterialCommunityIcons from './exports/MaterialCommunityIcons';
+import Octicons from './exports/Octicons';
+import SimpleLineIcons from './exports/SimpleLineIcons';
+import Zocial from './exports/Zocial';
+
 export {
   DangerZone,
   DeviceInfo,
   AppLoading,
   Asset,
   Font,
-  // Ionicons,
-  // Entypo,
-  // EvilIcons,
-  // Feather,
-  // FontAwesome,
-  // Foundation,
-  // MaterialIcons,
-  // MaterialCommunityIcons,
-  // Octicons,
-  // SimpleLineIcons,
-  // Zocial,
   Constants,
   Permissions,
   Notifications,
   Util,
   Localization,
   WebBrowser
+
+  // @expo/vector-icons
+  Ionicons,
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  Foundation,
+  MaterialIcons,
+  MaterialCommunityIcons,
+  Octicons,
+  SimpleLineIcons,
+  Zocial,
 };
